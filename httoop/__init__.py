15765c73726ff3c1412f9a2d93453eac7b831363
spaceone <space@wechall.net>
2013-05-23 15:04:22 +0200

    add wsgi stub

--- httoop/__init__.py
+++ httoop/__init__.py
@@ -4,11 +4,7 @@
 .. seealso:: :rfc:`2616`
 """
 
-# comment about coding style:
-# tabs for indentation because len('\t') == 1 and len('    ') == 4
-# also max(len(line)) == 120 because we have 1920px width, we are not in year 1980 anymore
-# if there are other PEP issues let me know!
-# feedback (about anything or any line) is welcome!!
+# TODO: PEP3101
 
 __all__ = ['Status', 'Body', 'Headers', 'URI', 'Method'
            'Request', 'Response', 'Protocol', 'Date',

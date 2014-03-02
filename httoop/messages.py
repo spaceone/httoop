7d1be4530bb7e6a3668be7183d8b155e4be79e98
spaceone <space@wechall.net>
2014-03-02 12:35:21 +0100

    separate HTTP logic from HTTP semantic

--- httoop/messages.py
+++ httoop/messages.py
@@ -10,10 +10,8 @@ import re
 
 from httoop.headers import Headers
 from httoop.status import Status
-from httoop.statuses import STATUSES
 from httoop.body import Body
 from httoop.uri import URI
-from httoop.date import Date
 from httoop.exceptions import InvalidLine
 from httoop.util import Unicode
 from httoop.meta import HTTPSemantic
@@ -113,34 +111,6 @@ class Message(object):
 	__metaclass__ = HTTPSemantic
 	__slots__ = ('__protocol', '__headers', '__body')
 
-	def __init__(self, protocol=None, headers=None, body=None):
-		u"""Initiates a new Message to hold information about the message.
-
-			:param protocol: the requested protocol
-			:type  protocol: str|tuple
-
-			:param headers: the request headers
-			:type  headers: dict or :class:`Headers`
-
-			:param body: the request body
-			:type  body: any
-		"""
-		self.__protocol = Protocol(protocol or (1, 1))
-		self.__headers = Headers(headers or {})
-		self.__body = Body(body or b'')
-
-	def parse(self, protocol):
-		u"""parses the HTTP protocol version
-
-			:param protocol: the protocol version string
-			:type  protocol: bytes
-		"""
-
-		self.protocol.parse(protocol)
-
-	def prepare(self):
-		pass
-
 	@property
 	def protocol(self):
 		return self.__protocol
@@ -165,25 +135,44 @@ class Message(object):
 	def body(self, body):
 		self.__body.set(body)
 
-	def compose_message(self):
-		u"""Compose the whole HTTP Message"""
-		start_line = bytes(self)
-		headers = bytes(self.headers)
-		body = bytes(self.body)
-		return b"%s%s%s" % (start_line, headers, body)
-
 	@property
-	def chunked(self):
-		return self.body.chunked or self.headers.get("Transfer-Encoding") == 'chunked'
+	def trailer(self):
+		return Headers((key, self.headers[key]) for key in self.headers.values('Trailer') if key in self.message.headers)
 
-	@chunked.setter
-	def chunked(self, chunked):
-		self.body.chunked = chunked
-		if chunked:
-			self.headers['Transfer-Encoding'] = 'chunked'
-			self.headers.pop('Content-Length', None)
-		else:
-			self.headers.pop('Transfer-Encoding', None)
+#	@trailer.setter
+#	def trailer(self, trailer):
+#		self.headers.pop('Trailer', None)
+#		if trailer:
+#			trailer = Headers(trailer)
+#			for key in trailer:
+#				self.headers.append('Trailer', key)
+#			self.headers.elements('Trailer')  # sanitize
+#			self.headers.merge(trailer)
+
+	def __init__(self, protocol=None, headers=None, body=None):
+		u"""Initiates a new Message to hold information about the message.
+
+			:param protocol: the requested protocol
+			:type  protocol: str|tuple
+
+			:param headers: the request headers
+			:type  headers: dict or :class:`Headers`
+
+			:param body: the request body
+			:type  body: any
+		"""
+		self.__protocol = Protocol(protocol or (1, 1))
+		self.__headers = Headers(headers or {})
+		self.__body = Body(body or b'')
+
+	def parse(self, protocol):
+		u"""parses the HTTP protocol version
+
+			:param protocol: the protocol version string
+			:type  protocol: bytes
+		"""
+
+		self.protocol.parse(protocol)
 
 	def __repr__(self):
 		return '<HTTP Message(protocol=%s)>' % (self.protocol,)
@@ -232,48 +221,25 @@ class Request(Message):
 			:param line: the request line
 			:type  line: bytes
 		"""
-		bits = line.split(None, 2)
-		if len(bits) != 3:
+		bits = line.strip().split(None, 2)
+		try:
+			method, uri, version = bits
+		except ValueError:
 			raise InvalidLine(line.decode('ISO8859-1'))
 
 		# protocol version
-		super(Request, self).parse(bits[2])
+		super(Request, self).parse(version)
 
 		# method
-		self.method.parse(bits[0])
+		self.method.parse(method)
 
 		# URI
-		self.uri.parse(bits[1])
-		self.uri.validate_http_request_uri()
+		self.uri.parse(uri)
+		self.uri.validate_http_request_uri()  # TODO: move the method into here
 
 	def compose(self):
 		u"""composes the request line"""
 		return b"%s %s %s\r\n" % (bytes(self.__method), bytes(self.__uri), bytes(self.protocol))
-
-	def prepare(self):
-		if self.method.safe:
-			self.body = None
-			self.chunked = False
-
-		self.chunked = self.chunked
-		self.close = self.close
-
-		if self.body and not self.chunked:
-			self.headers['Content-Length'] = bytes(len(self.body))
-
-		if self.method in ('PUT', 'POST') and self.body:
-			self.headers['Date'] = bytes(Date())  # RFC 2616 Section 14.18
-
-	@property
-	def close(self):
-		return self.headers.get('Connection') == 'close'
-
-	@close.setter
-	def close(self, close):
-		if close:
-			self.headers['Connection'] = 'close'
-		else:
-			self.headers.pop('Connection', None)
 
 	def __repr__(self):
 		return "<HTTP Request(%s %s %s)>" % (bytes(self.__method), bytes(self.__uri.path), bytes(self.protocol))
@@ -310,81 +276,21 @@ class Response(Message):
 	def parse(self, line):
 		u"""parses the response line"""
 
-		bits = line.split(None, 1)
-		if len(bits) != 2:
+		bits = line.strip().split(None, 1)
+		try:
+			version, status = bits
+		except ValueError:
 			raise InvalidLine(line.decode('ISO8859-1'))
 
 		# version
-		super(Response, self).parse(bits[0])
+		super(Response, self).parse(version)
 
 		# status
-		self.status.parse(bits[1])
+		self.status.parse(status)
 
 	def compose(self):
 		u"""composes the response line"""
 		return b"%s %s\r\n" % (bytes(self.protocol), bytes(self.status))
 
-	def prepare(self, request=None):
-		u"""prepares the response for being ready for transmitting"""
-
-		status = self.status
-		if status < 200 or status in (204, 205, 304):
-			# 1XX, 204 NO_CONTENT, 205 RESET_CONTENT, 304 NOT_MODIFIED
-			self.body = None
-
-		self.chunked = self.chunked
-		if not self.chunked:
-			self.headers['Content-Length'] = bytes(len(self.body))
-
-		self.headers['Date'] = bytes(Date())  # RFC 2616 Section 14.18
-
-		# remove header which should not occur along with this status
-		if int(status) in STATUSES:
-			for header in STATUSES[int(status)].header_to_remove:
-				self.headers.pop(header, None)
-
-		self.close = self.close
-
-		if self.body:
-			self.headers['Content-Type'] = bytes(self.body.mimetype)
-
-		if request is None:
-			return
-
-		if request.headers.get('Connection') == 'close':
-			self.close = True  # RFC 2616 Section 14.10
-
-		if request.method == u'HEAD':
-			self.body = None  # RFC 2616 Section 9.4
-
-	@property
-	def close(self):
-		# TODO: 100 Continue
-		if self.status == 413:
-			# 413 Request Entity Too Large
-			# RFC 2616 Section 10.4.14
-			pass
-		elif self.headers.get('Connection') == 'close':
-			pass
-		elif self.protocol < (1, 1):
-			pass
-		else:
-			return False
-		return True
-
-	@close.setter
-	def close(self, close):
-		if close:
-			if self.protocol >= (1, 1):
-				self.headers['Connection'] = 'close'
-				return
-		else:
-			if self.protocol < (1, 1):
-				self.headers['Connection'] = 'keep-alive'
-				return
-		self.headers.pop('Connection', None)
-
 	def __repr__(self):
-		# TODO: do we really want to check the body length here?
-		content_type = self.headers.get("Content-Type", "")
-		return "<HTTP Response(%d %s (%d))>" % (self.status, content_type, len(self.body))
+		return "<HTTP Response(%d %s)>" % (self.status, self.body.mimetype)

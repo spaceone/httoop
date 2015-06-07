c3c34a8395b640fd8f229884d5db602629a3625c
SpaceOne <space@wechall.net>
2015-06-08 01:24:28 +0200

    Fix version string

--- httoop/version.py
+++ httoop/version.py
@@ -3,6 +3,6 @@
 __version__ = 0.0
 from httoop.header import Server as __Server, UserAgent as __UserAgent
 from httoop.messages import Protocol
-UserAgentHeader = __UserAgent.parse('%s/%s' % (__name__, __version__))
-ServerHeader = __Server.parse('%s/%s' % (__name__, __version__))
+UserAgentHeader = __UserAgent.parse('httoop/%s' % (__version__,))
+ServerHeader = __Server.parse('httoop/%s' % (__version__,))
 ServerProtocol = Protocol((1, 1))

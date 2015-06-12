eb403aa6060c264c69afabbe9d92f71591d9a327
SpaceOne <space@wechall.net>
2015-06-12 23:54:56 +0200

    Don't use simplejson anymore

--- httoop/codecs/application/json.py
+++ httoop/codecs/application/json.py
@@ -3,13 +3,8 @@ from __future__ import absolute_import
 
 from httoop.codecs.common import Codec
 
-# TODO: http://stackoverflow.com/questions/712791/what-are-the-differences-between-json-and-simplejson-python-modules#answer-16131316
-try:
-	from simplejson import dumps as json_encode
-	from simplejson import loads as json_decode
-except ImportError:
-	from json import dumps as json_encode
-	from json import loads as json_decode
+from json import dumps as json_encode
+from json import loads as json_decode
 
 
 class JSON(Codec):

spaceone/httoop

commit 437209d1c8c2f8973020ad0f417156f35c034971

Fix IPv6 compatibility, improove Host header

httoop/uri/__init__.py
@@ -207,7 +207,10 @@ class URI(object):
 		path = b'/%s' % path
 		userinfo, _, hostport = authority.rpartition(b'@')
 		username, _, password = userinfo.partition(b':')
-		host, _, port = hostport.partition(b':')
+		if hostport.endswith(b']') and hostport.startswith(b'['):
+			host, port = hostport, b''
+		else:
+			host, _, port = hostport.rpartition(b':')
 
 		unquote = self.unquote
 		path = u'/'.join([unquote(seq).replace(u'/', u'%2f') for seq in path.split(b'/')])

httoop/__init__.py
@@ -49,5 +49,5 @@ from httoop.status import (
 
 try:
 	__import__('pkg_resources').declare_namespace(__name__)
-except ImportError:
+except ImportError:  # pragma: no cover
 	__path___ = __import__('pkgutil').extend_path(__path__, __name__)

httoop/authentication/digest.py
@@ -27,7 +27,7 @@ class DigestAuthScheme(object):
 		return b', '.join([HeaderElement.formatparam(k, v) for k, v in params])
 
 	@classmethod
-	def _compose(cls, authinfo):
+	def _compose(cls, authinfo):  # pragma: no cover
 		return authinfo
 
 	@classmethod

httoop/header/messaging.py
@@ -9,6 +9,7 @@ from httoop.exceptions import InvalidHeader
 class Accept(AcceptElement, MimeType):
 
 	def sanitize(self):
+		super(Accept, self).sanitize()
 		if self.value == '*':
 			self.value = '*/*'
 
@@ -115,44 +116,60 @@ class From(HeaderElement):
 
 
 # TODO: add case insensitve HeaderElement
-# TODO: integrate ipaddr.IP4/6Address which parses every form of ip addresses
 class Host(HeaderElement):
-	RE_HOSTNAME = re.compile(r'^([^\x00-\x1F\x7F()<>@,;:/\[\]={} \t\\\\"]+)(?::\d+)?$')
-	RE_IP4 = re.compile(r'^([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3})(?::\d+)?$')
-	# FIXME: implement IP parse, this matches 999.999.999.999
-	RE_IP6 = re.compile(r'^\[([0-9a-fA-F:]+)\](?::\d+)?$')
-	# FIXME: implement IP6 parse
+	RE_HOSTNAME = re.compile(r'^([^\x00-\x1F\x7F()^\'"<>@,;:/\[\]={} \t\\\\"]+)$')
+	HOSTPORT = re.compile(r'^(.*?)(?::(\d+))?$')
 
 	@property
 	def is_ip4(self):
-		return self.RE_IP4.match(self.value) is not None
+		from socket import inet_pton, AF_INET, error
+		try:
+			inet_pton(AF_INET, self.host)
+			return True
+		except error:
+			return False
 
 	@property
 	def is_ip6(self):
-		return self.RE_IP6.match(self.value) is not None
+		from socket import inet_pton, AF_INET6, error
+		try:
+			inet_pton(AF_INET6, self.host)
+			return True
+		except error:
+			return False
+
+	@property
+	def is_fqdn(self):
+		return not self.is_ip4 and not self.is_ip6 and self.RE_HOSTNAME.match(self.host) is not None
+
+	@property
+	def fqdn(self):
+		if self.is_fqdn:
+			return self.host
 
 	@property
 	def hostname(self):
-		return self.ip6address or \
-			self.ip4address or \
-			self.RE_HOSTNAME.match(self.value).group(1).lower()
+		return self.ip6address or self.ip4address or self.fqdn
 
 	@property
 	def ip6address(self):
-		# removes IPv6 brackets and port
 		if self.is_ip6:
-			return self.RE_IP6.match(self.value).group(1)
+			return self.host
 
 	@property
 	def ip4address(self):
-		# removes port
 		if self.is_ip4:
-			return self.RE_IP4.match(self.value).group(1)
+			return self.host
 
 	def sanitize(self):
-		if not self.RE_HOSTNAME.match(self.value):
-			raise InvalidHeader('Invalid Host header')
 		self.value = self.value.lower()
+		self.host, self.port = self.HOSTPORT.match(self.value).groups()
+		if self.host.endswith(']') and self.host.startswith('['):
+			self.host = self.host[1:-1]
+		if self.port:
+			self.port = int(self.port)
+		if not self.hostname:
+			raise InvalidHeader('Invalid Host header')
 
 
 class XForwardedHost(Host):

httoop/messages/body.py
@@ -223,6 +223,8 @@ class Body(IFile):
 
 		try:
 			for data in iterable:
+				if data is None:
+					continue
 				if isinstance(data, Unicode):
 					data = data.encode(encoding)
 				elif not isinstance(data, bytes):

commit 70c27a974a75b709e612cd5b91bd6a62c81b20cc

Fix URI-port parsing
regression introduced by 437209d1c8c2f8973020ad0f417156f35c034971

httoop/authentication/__init__.py
@@ -27,7 +27,7 @@ class AuthElement(HeaderElement):
 		try:
 			authinfo = parser.parse(authinfo)
 		except KeyError as key:
-			raise InvalidHeader(u'Missing paramter %r for authentication scheme %r' % (str(key), scheme))
+			raise InvalidHeader(u'Missing parameter %r for authentication scheme %r' % (str(key), scheme))
 
 		return scheme.title(), authinfo
 
@@ -40,7 +40,7 @@ class AuthElement(HeaderElement):
 		try:
 			authinfo = scheme.compose(self.params)
 		except KeyError as key:
-			raise InvalidHeader(u'Missing paramter %r for authentication scheme %r' % (str(key), self.value))
+			raise InvalidHeader(u'Missing parameter %r for authentication scheme %r' % (str(key), self.value))
 
 		return b'%s %s' % (self.value.title(), authinfo)
 

httoop/header/headers.py
@@ -82,8 +82,7 @@ class Headers(CaseInsensitiveDict):
 			self[key] = Element.merge(self.elements(key), other.elements(key))
 
 	def set(self, headers):
-		for key in self.keys():
-			del self[key]
+		self.clear()
 		self.update(headers)
 
 	def parse(self, data):

httoop/uri/__init__.py
@@ -71,7 +71,7 @@ class URI(object):
 				if not 0 <= int(port) <= 65535:
 					raise ValueError
 			except ValueError:
-				raise InvalidURI('Invalid port: %r' % (port))  # TODO: TypeError
+				raise InvalidURI('Invalid port: %r' % (port,))  # TODO: TypeError
 		self._port = port
 
 	def __init__(self, uri=None, *args, **kwargs):
@@ -210,8 +210,10 @@ class URI(object):
 		username, _, password = userinfo.partition(b':')
 		if hostport.endswith(b']') and hostport.startswith(b'['):
 			host, port = hostport, b''
-		else:
+		elif ':' in hostport:
 			host, _, port = hostport.rpartition(b':')
+		else:
+			host, port = hostport, b''
 
 		unquote = self.unquote
 		path = u'/'.join([unquote(seq).replace(u'/', u'%2f') for seq in path.split(b'/')])
3dd472fbfe30e5f711555e49ab8072640533f8ba
SpaceOne <space@wechall.net>
2015-12-10 06:52:15 +0100

    Add test for Method.__hash__

--- tests/api/test_method.py
+++ tests/api/test_method.py
@@ -1,4 +1,5 @@
 # See also: tests/request_/test_request_method.py
+import pytest
 
 def test_safe_methods(request_):
 	all_methods = (
@@ -14,3 +15,17 @@ def test_safe_methods(request_):
 		request_.method = method
 		assert request_.method.safe == safe
 		assert request_.method.idempotent == idempotent
+
+
+@pytest.mark.xfail(reason='hash changing + fixed references')
+def test_hashable_methods(request_):
+	methods = {}
+	request_.method = u'GET'
+	methods[request_.method] = 1
+	assert 'GET' in methods
+	assert 'POST' not in methods
+	request_.method = u'POST'
+	assert 'POST' not in methods
+	methods[request_.method] = 1
+	assert 'POST' in methods
+	assert 'GET' in methods

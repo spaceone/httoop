f2ed3622e9c502f32e34f628f714b6acc7dd29f2
SpaceOne <space@wechall.net>
2017-07-24 13:41:45 +0200

    use defusedxml for security purposes

--- httoop/codecs/application/xml.py
+++ httoop/codecs/application/xml.py
@@ -4,7 +4,10 @@ from __future__ import absolute_import
 
 # TODO: http://docs.python.org/2/library/xml.html#xml-vulnerabilities
 # TODO: https://www.owasp.org/index.php/XML_External_Entity_%28XXE%29_Processing
-from xml.etree.ElementTree import parse, ParseError, tostring
+try:
+	from defusedxml.ElementTree import parse, ParseError, tostring
+except ImportError:
+	from xml.etree.ElementTree import parse, ParseError, tostring
 
 from httoop.codecs.codec import Codec
 from httoop.exceptions import DecodeError

951a54755cadb4ab33fa7421d9b9f1c425538edb
SpaceOne <space@wechall.net>
2018-12-20 17:45:15 +0100

    Improove wsgi gateway component

--- httoop/gateway/wsgi.py
+++ httoop/gateway/wsgi.py
@@ -26,8 +26,9 @@ class WSGIBody(Body):  # pragma: no cover
 class WSGI(object):  # pragma: no cover
 	u"""A mixin class which implements the WSGI interface"""
 
-	def __init__(self, *args, **kwargs):
+	def __init__(self, use_path_info=False, *args, **kwargs):
 		super(WSGI, self).__init__(*args, **kwargs)
+		self.use_path_info = use_path_info
 		self.wsgi_version = (1, 0)
 		self.errors = sys.stderr
 		self.multithread = False
@@ -52,7 +53,8 @@ class WSGI(object):  # pragma: no cover
 		self.start_response(application(environ, start_response))
 
 	def start_response(self, response_body):
-		self.response.body = response_body
+		if response_body:
+			self.response.body = response_body
 
 	def _get_environ(self):
 		environ = {}
@@ -65,6 +67,7 @@ class WSGI(object):  # pragma: no cover
 			'REQUEST_METHOD': bytes(self.request.method),
 			'SCRIPT_NAME': b'',
 			'PATH_INFO': bytes(self.request.uri.path),
+			'REQUEST_URI': bytes(self.request.uri.path),
 			'QUERY_STRING': bytes(self.request.uri.query_string),
 			'CONTENT_TYPE': self.request.headers.get('Content-Type'),
 			'CONTENT_LENGTH': self.request.headers.get('Content-Length'),
@@ -85,24 +88,35 @@ class WSGI(object):  # pragma: no cover
 		environ = os.environ if environ is None else environ
 		environ = environ.copy()
 
-		for name, value in iteritems(environ):
+		for name, value in list(environ.items()):
 			if name.startswith('HTTP_'):
+				environ.pop(name)
 				self.request.headers[name[5:].replace('_', '-')] = value
 
-		self.request.body = environ['REQUEST_METHOD']
-		self.request.uri.path = environ['PATH_INFO']
-		self.request.uri.query_string = environ['QUERY_STRING']
-		self.request.headers['Content-Type'] = environ.get('CONTENT_TYPE')
-		self.request.headers['Content-Length'] = environ.get('Content-Length')
-		self.server_name = environ.get('SERVER_NAME')
-		self.server_port = environ.get('SERVER_PORT')
-		self.request.protocol = environ['SERVER_PROTOCOL']
-		self.wsgi_version = environ['wsgi.version']
-		self.request.uri.scheme = environ['wsgi.url_scheme']
-		self.request.body = environ['wsgi.input']
-		self.errors = environ['wsgi.errors']
-		self.multithread = environ['wsgi.multithread']
-		self.multiprocess = environ['wsgi.multiprocess']
-		self.run_once = environ['wsgi.run_once']
+		self.request.method = environ.pop('REQUEST_METHOD')
+		if self.use_path_info:
+			self.request.uri.path = environ.pop('PATH_INFO')
+		else:
+			self.request.uri.path = environ.pop('REQUEST_URI')
+		self.request.uri.scheme = environ.pop('REQUEST_SCHEME')
+		self.request.uri.query_string = environ.pop('QUERY_STRING')
+		if 'CONTENT_TYPE' in environ:
+			self.request.headers['Content-Type'] = environ.pop('CONTENT_TYPE')
+		if 'CONTENT_LENGTH' in environ:
+			self.request.headers['Content-Length'] = environ.pop('CONTENT_LENGTH')
+		self.server_name = environ.pop('SERVER_NAME', None)
+		self.server_address = environ.pop('SERVER_ADDR', None)
+		self.server_port = environ.pop('SERVER_PORT', None)
+		self.request.protocol = environ.pop('SERVER_PROTOCOL')
+		self.wsgi_version = environ.pop('wsgi.version')
+		self.request.uri.scheme = environ.pop('wsgi.url_scheme')
+		self.request.body = environ.pop('wsgi.input').read()
+		self.request.body.seek(0)
+		self.errors = environ.pop('wsgi.errors')
+		self.multithread = environ.pop('wsgi.multithread')
+		self.multiprocess = environ.pop('wsgi.multiprocess')
+		self.run_once = environ.pop('wsgi.run_once')
+		self.remote_address = environ.pop('REMOTE_ADDR')
+		self.remote_port = environ.pop('REMOTE_PORT')
 
 		self.environ = environ

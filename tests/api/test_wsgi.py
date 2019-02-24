ea20908e7f1fa46b4bcd1250a31952925c5018ee
SpaceOne <space@wechall.net>
2019-02-24 13:28:17 +0100

    Fix WSGI test

--- tests/api/test_wsgi.py
+++ tests/api/test_wsgi.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 import pytest
+import sys
 
 from httoop import Request, Response
 from httoop.gateway.wsgi import WSGI
@@ -128,13 +129,23 @@ def test_eror_reraising():
 
 
 def application15(environ, start_response):
-	print environ
 	result = environ['HTTP_HOST'] == 'foobar' and environ['CONTENT_LENGTH'] == '0' and environ['CONTENT_TYPE'] == 'text/html'
-	raise ValueError(result)
+	try:
+		raise ValueError(result)
+	except ValueError:
+		start_response('200 OK', {}, sys.exc_info())
+	return []
 
 
 def test_essential_parameters():
 	client = WSGIClient({'CONTENT_TYPE': 'text/html', 'CONTENT_LENGTH': '0', 'HTTP_HOST': 'foobar'})
+	client(application15)
+	assert client.exc_info[1].args[0] is True
+
+
+def test_client_reraising():
+	client = WSGIClient({'CONTENT_TYPE': 'text/html', 'CONTENT_LENGTH': '0', 'HTTP_HOST': 'foobar'})
+	client.headers_sent = True
 	with pytest.raises(ValueError) as exc:
 		client(application15)
 	assert exc.value.args[0] is True

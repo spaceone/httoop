spaceone/httoop

commit 99f5f51a6ebab4bfdfd02d3705a0bffb5379b4a9

fix[gzip]: never write duplicated gzip header with iterencode()

httoop/codecs/codec.py
@@ -19,8 +19,10 @@ class Codec(object):
 
 	@classmethod
 	def iterencode(cls, data, charset=None, mimetype=None):  # pragma: no cover
-		return cls.encode(data, charset, mimetype)
+		for part in data:
+			yield cls.encode(part, charset, mimetype)
 
 	@classmethod
 	def iterdecode(cls, data, charset=None, mimetype=None):  # pragma: no cover
-		return cls.decode(data, charset, mimetype)
+		for part in data:
+			yield cls.decode(part, charset, mimetype)

httoop/codecs/application/gzip.py
@@ -20,10 +20,9 @@ class GZip(Codec):
 	def encode(cls, data, charset=None, mimetype=None):
 		try:
 			out = io.BytesIO()
-			with gzip.GzipFile(fileobj=out, mode="w") as fd:
+			with gzip.GzipFile(fileobj=out, mode="w", compresslevel=cls.compression_level) as fd:
 				fd.write(Codec.encode(data, charset))
 			return out.getvalue()
-			# return zlib.compress(Codec.encode(data, charset), cls.compression_level)
 		except zlib.error:  # pragma: no cover
 			raise EncodeError(_(u'Invalid gzip data.'))
 
@@ -32,7 +31,39 @@ class GZip(Codec):
 		try:
 			with gzip.GzipFile(fileobj=io.BytesIO(data)) as fd:
 				data = fd.read()
-			# data = zlib.decompress(data, 16 + zlib.MAX_WBITS)
 		except (zlib.error, IOError, EOFError):
 			raise DecodeError(_(u'Invalid gzip data.'))
 		return Codec.decode(data, charset)
+
+	@classmethod
+	def iterencode(cls, data, charset=None, mimetype=None):
+		try:
+			out = io.BytesIO()
+			with gzip.GzipFile(fileobj=out, mode="w", compresslevel=cls.compression_level) as fd:
+				for part in data:
+					fd.write(Codec.encode(part, charset))
+					yield out.getvalue()
+					out.seek(0)
+					out.truncate()
+			yield out.getvalue()
+		except zlib.error:  # pragma: no cover
+			raise EncodeError(_(u'Invalid gzip data.'))
+
+	@classmethod
+	def iterdecode(cls, data, charset=None, mimetype=None):
+		try:
+			fd = io.BytesIO()
+			with gzip.GzipFile(fileobj=fd) as gzfd:
+				# FIXME: the gzip module cannot handle partial data
+				#for part in data:
+				#	fd.write(part)
+				#	fd.seek(fd.tell() - length)
+				#	fd.seek(fd.tell() - length)
+				#	yield Codec.decode(gzfd.read(), charset)
+				#yield Codec.decode(gzfd.read(), charset)
+				for part in data:
+					fd.write(part)
+				fd.seek(0)
+				yield Codec.decode(gzfd.read(), charset)
+		except (zlib.error, IOError, EOFError):
+			raise DecodeError(_(u'Invalid gzip data.'))

commit e00a81066d60161674b288a72c6050b6f4abcd67

fix(codecs): fix fallback Codec.iterencode
empty sequences like dicts were never evaluated by inheriting codec
classes such as application/json.

FAILED tests/api/test_body.py::test_body_iterencode - AssertionError: assert b'' == b'{}'

  Full diff:
  - b'{}'
  ?   --
  + b''

Fixes: 99f5f51a6ebab4bfdfd02d3705a0bffb5379b4a9

httoop/codecs/codec.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from typing import Any, Dict, Optional
+from typing import Any
 
 from httoop.util import Unicode
 
@@ -20,11 +20,9 @@ class Codec(object):
         return data
 
     @classmethod
-    def iterencode(cls, data: Dict[Any, Any], charset: Optional[str] = None, mimetype: Optional["ContentType"] = None) -> None:  # pragma: no cover
-        for part in data:
-            yield cls.encode(part, charset, mimetype)
+    def iterencode(cls, data: Any, charset: str | None = None, mimetype: ContentType | None = None) -> None:  # pragma: no cover
+        yield cls.encode(data, charset, mimetype)
 
     @classmethod
-    def iterdecode(cls, data, charset=None, mimetype=None):  # pragma: no cover
-        for part in data:
-            yield cls.decode(part, charset, mimetype)
+    def iterdecode(cls, data: Any, charset=None, mimetype=None):  # pragma: no cover
+        yield cls.decode(data, charset, mimetype)
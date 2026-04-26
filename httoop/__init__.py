87165ff039e258f2fa61b1a9ea47fa5c48ded9ac
Space One <space@wechall.net>
2026-04-27 00:19:12 +0200

    feat: drop namespacing hooks
    pkg_resources is deprecated.
    Since Python 3.3 there is native namespacing.
    But this requires that no __init__.py exists, therefore not usable here.
    
    in case we need it, e.g. to support httoop-http2 we can still use
    `__path___ = __import__('pkgutil').extend_path(__path__, __name__)`.

--- httoop/__init__.py
+++ httoop/__init__.py
@@ -51,8 +51,3 @@ __all__ = [
     '__version__', 'ServerProtocol', 'UserAgentHeader', 'ServerHeader',
     'cache', 'ComposedRequest', 'ComposedResponse',
 ]
-
-try:
-    __import__('pkg_resources').declare_namespace(__name__)
-except ImportError:  # pragma: no cover
-    __path___ = __import__('pkgutil').extend_path(__path__, __name__)

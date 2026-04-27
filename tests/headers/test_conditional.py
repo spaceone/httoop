8a3bc48bb45b91dfec8c32d6da02d7ef46e916d6
Space One <space@wechall.net>
2026-04-27 21:24:10 +0200

    test(headers): test If-Match conditionals
    Fixes: 34e47399cbfbd6c0b5ac493ce7ee0867197955db

--- tests/headers/test_conditional.py
+++ tests/headers/test_conditional.py
@@ -1,3 +1,6 @@
+import pytest
+
+
 def test_conditional_header(headers):
     headers.parse(b'Last-Modified: Mon, 15 Jun 2020 21:18:41 GMT')
     headers.parse(b'If-Modified-Since: Mon, 15 Jun 2020 21:18:40 GMT')
@@ -16,3 +19,54 @@ def test_etag_header(headers):
     assert headers.element('ETag') == 'foo'
     assert headers.element('ETag') == '*'
     assert headers.element('ETag') != 'bar'
+
+
+def test_if_match_header(headers):
+    headers.parse(b'If-Match: W/"foo"')
+    assert headers.element('If-Match') == 'W/"foo"'
+    assert headers.element('If-Match').matches('W/"foo"')
+    assert headers.element('If-Match') != '"foo"'
+    assert headers.element('If-Match') != 'foo'
+    assert headers.element('If-Match') != '*'
+    assert headers.element('If-Match').matches_etag('foo', strong=False)
+    assert not headers.element('If-Match').matches_etag('foo')
+
+
+def test_if_match_header_star(headers):
+    headers.parse(b'If-Match: *')
+    assert headers.element('If-Match') == 'W/"foo"'
+
+
+@pytest.mark.parametrize('match', [
+    '"def456"',
+    '"abc123", "def456", "ghi789"',
+    '*',
+])
+def test_if_match_header_raw(headers, match):
+    etag = 'def456'
+    headers.parse(f'If-Match: {match}'.encode())
+    assert any(condition.matches_etag(etag) for condition in headers.elements('If-Match'))
+
+
+@pytest.mark.parametrize('match', [
+    '"def456"',
+    'W/"def456"',
+    '"abc123", "def456", "ghi789"',
+    '*',
+])
+def test_if_none_match_header_raw(headers, match):
+    etag = 'def456'
+    headers.parse(f'If-Match: {match}'.encode())
+    assert any(condition.matches_etag(etag, strong=False) for condition in headers.elements('If-Match'))
+
+
+@pytest.mark.parametrize('match', [
+    'def456',
+    'w/"def456"',
+    'W/"def456"',
+    '"abc123", "ghi789"',
+])
+def test_if_mismatch_header_raw(headers, match):
+    etag = 'def456'
+    headers.parse(f'If-Match: {match}'.encode())
+    assert not any(condition.matches_etag(etag) for condition in headers.elements('If-Match'))

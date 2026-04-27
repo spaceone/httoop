spaceone/httoop

commit 34e47399cbfbd6c0b5ac493ce7ee0867197955db

feat: Allow to compare If-Match/If-None-Match against *

httoop/header/conditional.py
@@ -27,6 +27,12 @@ class _DateComparable(object):
 		return int(self.value)
 
 
+class _MatchElement(object):
+
+	def matches(self, value):
+		return self == value or self == '*'
+
+
 class ETag(HeaderElement):
 
 	is_response_header = True
@@ -42,7 +48,7 @@ class LastModified(_DateComparable, HeaderElement):
 	is_response_header = True
 
 
-class IfMatch(HeaderElement):
+class IfMatch(_MatchElement, HeaderElement):
 	__name__ = 'If-Match'
 	is_request_header = True
 
@@ -52,7 +58,7 @@ class IfModifiedSince(_DateComparable, HeaderElement):
 	is_request_header = True
 
 
-class IfNoneMatch(HeaderElement):
+class IfNoneMatch(_MatchElement, HeaderElement):
 	__name__ = 'If-None-Match'
 	is_request_header = True
 

commit ba668a4535a8361dd9b747acd1ea7930545c9f7b

feat(header.conditional): allow to compare If-Match with raw E-Tags
Fixes: 34e47399cbfbd6c0b5ac493ce7ee0867197955db

httoop/header/conditional.py
@@ -30,8 +30,28 @@ class _DateComparable:
 
 class _MatchElement:
 
-    def matches(self, value):
-        return self == value or self == '*'
+    def __eq__(self, other: Any) -> bool:
+        return self.value == other or self.value == '*'
+
+    def matches(self, etag):
+        return self == etag
+
+    def matches_etag(self, etag, *, strong: bool = True):
+        value = self.value
+
+        if value == '*':
+            return True
+
+        is_weak = value.startswith('W/')
+        if is_weak:
+            if strong:
+                return False
+            value = value[2:]
+
+        if not (value.startswith('"') and value.endswith('"')):
+            return False
+
+        return value[1:-1] == etag
 
 
 class ETag(HeaderElement):
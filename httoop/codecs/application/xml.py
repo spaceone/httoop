spaceone/httoop

commit a03b734de6cc13bbb6e4286bce81f715b788667d

chore: add type annotations

httoop/codecs/application/xml.py
@@ -5,14 +5,16 @@ from __future__ import absolute_import
 # TODO: http://docs.python.org/2/library/xml.html#xml-vulnerabilities
 # TODO: https://www.owasp.org/index.php/XML_External_Entity_%28XXE%29_Processing
 try:
-	from defusedxml.ElementTree import ParseError, fromstring, tostring
+	from defusedxml.ElementTree import Element, ParseError, fromstring, tostring
 except ImportError:  # pragma: no cover
 	# TODO: emit a warning
-	from xml.etree.ElementTree import ParseError, tostring  # nosec
+	from xml.etree.ElementTree import Element, ParseError, tostring  # nosec
 
 	def fromstring(data):
 		raise ParseError('Will not parse without defusedxml!')
 
+
+from typing import Optional
 
 from httoop.codecs.codec import Codec
 from httoop.exceptions import DecodeError
@@ -22,12 +24,12 @@ class XML(Codec):
 	mimetype = 'application/xml'
 
 	@classmethod
-	def decode(cls, data, charset=None, mimetype=None):
+	def decode(cls, data: bytes, charset: Optional[str]=None, mimetype: Optional["ContentType"]=None) -> Element:
 		try:
 			return fromstring(data)
 		except ParseError as exc:
 			raise DecodeError(u'Could not decode as %s: %s' % (mimetype, exc,))
 
 	@classmethod
-	def encode(cls, root, charset=None, mimetype=None):
+	def encode(cls, root: Element, charset: Optional[str]=None, mimetype: Optional["ContentType"]=None) -> bytes:
 		return tostring(root, charset)

httoop/__main__.py
@@ -70,7 +70,7 @@ class CLI(object):
 			self.arguments.file = self.arguments.file.buffer
 		self.arguments.func()
 
-	def add_common_arguments(self, add):
+	def add_common_arguments(self, add) -> None:
 		add('--protocol')
 		add('-H', '--header', action='append', default=[])
 		add('-b', '--body', default='')

httoop/authentication/__init__.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import re
+from typing import Any, Dict, List, Tuple, Union
 
 from httoop.authentication.basic import BasicAuthRequestScheme, BasicAuthResponseScheme
 from httoop.authentication.digest import DigestAuthRequestScheme, DigestAuthResponseScheme
@@ -13,7 +14,7 @@ class AuthElement(HeaderElement):
 	schemes = {}
 	RE_SPACE_SPLIT = re.compile(br'\s+(?=(?:[^"]*"[^"]*")*[^"]*$)')
 
-	def sanitize(self):
+	def sanitize(self) -> None:
 		for key, value in list(self.params.items()):
 			if not isinstance(value, bytes) and isinstance(value, str):
 				self.params[key] = value.encode('UTF-8')
@@ -21,7 +22,7 @@ class AuthElement(HeaderElement):
 				self.params[key] = type(value)(x.encode('UTF-8') if not isinstance(x, bytes) and isinstance(x, str) else x for x in value)
 
 	@classmethod
-	def parseparams(cls, elementstr):
+	def parseparams(cls, elementstr: bytes) -> Union[Tuple[bytes, Dict[bytes, str]], Tuple[bytes, Dict[str, bytes]], Tuple[bytes, Dict[str, Union[bytes, List[bytes], bool]]], Tuple[bytes, Dict[str, Union[bytes, List[bytes]]]], Tuple[bytes, Dict[bytes, Union[str, bytes]]]]:
 		try:
 			scheme, authinfo = elementstr.split(b' ', 1)
 		except ValueError:
@@ -38,7 +39,7 @@ class AuthElement(HeaderElement):
 
 		return scheme.title(), authinfo
 
-	def compose(self):
+	def compose(self) -> bytes:
 		try:
 			scheme = self.schemes[self.value.lower()]
 		except KeyError:
@@ -52,7 +53,7 @@ class AuthElement(HeaderElement):
 		return b'%s %s' % (self.value.encode('ASCII').title(), authinfo)
 
 	@classmethod
-	def split(cls, value):
+	def split(cls, value: bytes) -> List[Union[bytes, Any]]:
 		value = cls.RE_SPACE_SPLIT.split(value)
 		indexes = [i for i, val in enumerate(value) if val != b',' and b'=' not in val]
 		return [b' '.join(value[a:b]) for a, b in zip(indexes, indexes[1:] + [None])]
@@ -98,11 +99,11 @@ class AuthResponseElement(AuthElement):
 	}
 
 	@classmethod
-	def sorted(cls, elements):
+	def sorted(cls, elements: List[Union["WWWAuthenticate", Any]]) -> List[Union["WWWAuthenticate", Any]]:
 		return list(sorted(elements, key=lambda e: {'basic': u'\xff'}.get(e.value.lower(), e.value)))
 
 	@classmethod
-	def join(cls, values):
+	def join(cls, values: List[bytes]) -> bytes:
 		return b' '.join(values)
 
 	@property

httoop/authentication/basic.py
@@ -1,16 +1,17 @@
 # -*- coding: utf-8 -*-
 
 from binascii import Error as Base64Error
+from typing import Dict, Union
 
 from httoop.exceptions import InvalidHeader
 from httoop.header.element import HeaderElement
-from httoop.util import _, decode_base64, encode_base64
+from httoop.util import ByteUnicodeDict, _, decode_base64, encode_base64
 
 
 class BasicAuthRequestScheme(object):
 
 	@staticmethod
-	def parse(authinfo):
+	def parse(authinfo: bytes) -> Dict[str, bytes]:
 		#try:
 		#	authinfo = authinfo.encode('ascii')
 		#except ValueError:
@@ -32,7 +33,7 @@ class BasicAuthRequestScheme(object):
 		return authinfo
 
 	@staticmethod
-	def compose(authinfo):
+	def compose(authinfo: ByteUnicodeDict) -> bytes:
 		username = authinfo['username']
 		password = authinfo['password']
 		#username = username.encode('ISO8859-1')
@@ -43,11 +44,11 @@ class BasicAuthRequestScheme(object):
 class BasicAuthResponseScheme(object):
 
 	@staticmethod
-	def parse(authinfo):
+	def parse(authinfo: bytes) -> Dict[bytes, Union[str, bytes]]:
 		params = HeaderElement.parseparams(b'X;%s' % authinfo)[1]
 		params.setdefault(b'realm', b'')
 		return params
 
 	@staticmethod
-	def compose(authinfo):
+	def compose(authinfo: ByteUnicodeDict) -> bytes:
 		return HeaderElement.formatparam(b'realm', authinfo['realm'], True)

httoop/authentication/digest.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from hashlib import md5, sha256
+from typing import Callable, Dict, List, Tuple, Union
 
 from httoop.exceptions import InvalidHeader
 from httoop.header.element import HeaderElement
@@ -20,14 +21,14 @@ class DigestAuthScheme(object):
 	qops = (b'auth', b'auth-int')  # quality of protection
 
 	@classmethod
-	def get_algorithm(cls, algorithm):
+	def get_algorithm(cls, algorithm: Union[bytes, str]) -> Callable:
 		try:
 			return cls.algorithms[algorithm.decode('ASCII', 'ignore') if isinstance(algorithm, bytes) else algorithm]
 		except KeyError:
 			raise InvalidHeader(_(u'Unknown digest authentication algorithm: %r'), algorithm)
 
 	@classmethod
-	def generate_nonce(cls, authinfo):
+	def generate_nonce(cls, authinfo: ByteUnicodeDict) -> bytes:
 		from time import time
 		from uuid import uuid4
 		nonce = b'%d:%s:%s' % (time(), authinfo.get('etag', authinfo.get('realm', b'')), str(uuid4()).encode('ASCII'), )
@@ -36,7 +37,7 @@ class DigestAuthScheme(object):
 		return H(nonce)
 
 	@classmethod
-	def compose(cls, authinfo):
+	def compose(cls, authinfo: ByteUnicodeDict) -> bytes:
 		params = cls._compose(authinfo)
 		return b', '.join([HeaderElement.formatparam(k.encode('ASCII'), v) for k, v in params])
 
@@ -45,7 +46,7 @@ class DigestAuthScheme(object):
 		return authinfo
 
 	@classmethod
-	def parse(cls, authinfo):
+	def parse(cls, authinfo: bytes) -> ByteUnicodeDict:
 		atoms = [x.strip() for x in authinfo.split(b',') if x.strip()] or [b'']
 
 		params = dict((key.strip(), value.strip().strip(b'"')) for key, _, value in (atom.partition(b'=') for atom in atoms))
@@ -55,7 +56,7 @@ class DigestAuthScheme(object):
 class DigestAuthResponseScheme(DigestAuthScheme):  # WWW-Authenticate
 
 	@classmethod
-	def _compose(cls, authinfo):
+	def _compose(cls, authinfo: ByteUnicodeDict) -> List[Tuple[str, bytes]]:
 		realm = authinfo['realm']
 		algorithm = authinfo.get('algorithm', b'MD5')
 		domain = authinfo.get('domain')
@@ -84,7 +85,7 @@ class DigestAuthResponseScheme(DigestAuthScheme):  # WWW-Authenticate
 		return [(k, v) for k, v in params if v is not None]
 
 	@classmethod
-	def parse(cls, authinfo):
+	def parse(cls, authinfo: bytes) -> Dict[str, Union[bytes, List[bytes], bool]]:
 		params = super(cls, cls).parse(authinfo)
 		if b'"' in params['nonce']:
 			raise InvalidHeader(_(u'Nonce must not contain double quote'))
@@ -106,7 +107,7 @@ class DigestAuthResponseScheme(DigestAuthScheme):  # WWW-Authenticate
 class DigestAuthRequestScheme(DigestAuthScheme):  # Authorization
 
 	@classmethod
-	def _compose(cls, authinfo):
+	def _compose(cls, authinfo: ByteUnicodeDict) -> List[Tuple[str, bytes]]:
 		username = authinfo['username']
 		realm = authinfo['realm']
 		digest_uri = authinfo['uri']
@@ -135,7 +136,7 @@ class DigestAuthRequestScheme(DigestAuthScheme):  # Authorization
 		return [(k, v) for k, v in params if v is not None]
 
 	@classmethod
-	def parse(cls, authinfo):
+	def parse(cls, authinfo: bytes) -> Dict[str, bytes]:
 		params = super(cls, cls).parse(authinfo)
 		message_qop = params.get('qop')
 		cnonce = None
@@ -158,14 +159,14 @@ class DigestAuthRequestScheme(DigestAuthScheme):  # Authorization
 		return dict([(k, v) for k, v in params if v is not None])
 
 	@classmethod
-	def check(cls, authinfo, request_params):
+	def check(cls, authinfo: ByteUnicodeDict, request_params: ByteUnicodeDict) -> bool:
 		if authinfo['realm'] != request_params['realm']:
 			return False
 		response = cls.calculate_request_digest(authinfo)
 		return response == request_params['response']
 
 	@classmethod
-	def calculate_request_digest(cls, authinfo):
+	def calculate_request_digest(cls, authinfo: ByteUnicodeDict) -> bytes:
 		algorithm = authinfo.get('algorithm', b'MD5').decode('ASCII', 'replace')
 		H = cls.get_algorithm(algorithm)
 
@@ -186,7 +187,7 @@ class DigestAuthRequestScheme(DigestAuthScheme):  # Authorization
 		return H(b'%s:%s' % (secret, data))
 
 	@classmethod
-	def A2(cls, params):
+	def A2(cls, params: ByteUnicodeDict) -> bytes:
 		qop = params.get('qop', b'')
 		if not qop or qop == b'auth':
 			return b'%s:%s' % (params['method'], params['uri'])
@@ -197,7 +198,7 @@ class DigestAuthRequestScheme(DigestAuthScheme):  # Authorization
 			raise NotImplementedError('Unknown quality of protection: %r' % (qop, ))
 
 	@classmethod
-	def A1(cls, params):
+	def A1(cls, params: ByteUnicodeDict) -> bytes:
 		algorithm = params.get('algorithm', b'')
 
 		if not algorithm or algorithm == b'MD5':

httoop/client/__init__.py
@@ -7,11 +7,11 @@ class ClientStateMachine(StateMachine):
 
 	Message = Response
 
-	def on_headers_complete(self):
+	def on_headers_complete(self) -> None:
 		super(ClientStateMachine, self).on_headers_complete()
 		self.remove_invalid_headers()
 
-	def remove_invalid_headers(self):
+	def remove_invalid_headers(self) -> None:
 		if self.request.method == u'CONNECT':
 			self.message.headers.pop('Transfer-Encoding', None)
 			self.message.headers.pop('Content-Length', None)

httoop/codecs/__init__.py
@@ -4,6 +4,7 @@ common used in combination with HTTP.
 """
 
 import inspect
+from typing import Any, Type
 
 from httoop.codecs import application, audio, example, image, message, model, multipart, text, video
 from httoop.codecs.codec import Codec
@@ -18,12 +19,12 @@ __all__ = [
 ]
 
 
-def lookup(encoding, raise_errors=True):
+def lookup(encoding: str, raise_errors: bool=True) -> Any:
 	type_ = '%s/*' % (encoding.split('/', 1)[0], )
 	return CODECS.get(encoding) or CODECS.get(type_) or (raise_errors and CODECS[encoding]) or None
 
 
-def register(encoding, codec):
+def register(encoding: str, codec: Type["Codec"]) -> None:
 	CODECS[encoding] = codec
 
 

httoop/codecs/application/gzip.py
@@ -17,7 +17,7 @@ class GZip(Codec):
 	compression_level = 6
 
 	@classmethod
-	def encode(cls, data, charset=None, mimetype=None):
+	def encode(cls, data: bytes, charset: None=None, mimetype: None=None) -> bytes:
 		try:
 			out = io.BytesIO()
 			with gzip.GzipFile(fileobj=out, mode="w", compresslevel=cls.compression_level) as fd:
@@ -27,7 +27,7 @@ class GZip(Codec):
 			raise EncodeError(_(u'Invalid gzip data.'))
 
 	@classmethod
-	def decode(cls, data, charset=None, mimetype=None):
+	def decode(cls, data: bytes, charset: None=None, mimetype: None=None) -> str:
 		try:
 			with gzip.GzipFile(fileobj=io.BytesIO(data)) as fd:
 				data = fd.read()

commit 5c983ce80fd6b8cb773f9bb650587f8bc83c06d6

fix(httoop.codecs.application.xml): fix importing of Element for type checking
Fixes: a03b734de6cc13bbb6e4286bce81f715b788667d

httoop/codecs/application/xml.py
@@ -1,14 +1,22 @@
 # -*- coding: utf-8 -*-
 
 from __future__ import absolute_import
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+
+if TYPE_CHECKING:
+    from xml.etree.ElementTree import Element
+
 
 # TODO: http://docs.python.org/2/library/xml.html#xml-vulnerabilities
 # TODO: https://www.owasp.org/index.php/XML_External_Entity_%28XXE%29_Processing
 try:
-    from defusedxml.ElementTree import Element, ParseError, fromstring, tostring
+    from defusedxml.ElementTree import ParseError, fromstring, tostring
 except ImportError:  # pragma: no cover
     # TODO: emit a warning
-    from xml.etree.ElementTree import Element, ParseError, tostring  # nosec
+    from xml.etree.ElementTree import ParseError, tostring
 
     def fromstring(data):
         raise ParseError('Will not parse without defusedxml!')
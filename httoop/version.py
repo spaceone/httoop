ae9eb292d3099dcfbc3e9d37e8c2e59d7f13480f
Space One <space@wechall.net>
2026-04-29 03:17:25 +0200

    chore: publish 0.2 release

--- httoop/version.py
+++ httoop/version.py
@@ -2,7 +2,7 @@ from httoop.header import Server as __Server, UserAgent as __UserAgent
 from httoop.messages import Protocol
 
 
-__version__ = '0.1.1'
+__version__ = '0.2.0'
 UserAgentHeader = __UserAgent.parse(b'httoop/%s' % (__version__.encode(),))
 ServerHeader = __Server.parse(b'httoop/%s' % (__version__.encode(),))
 ServerProtocol = Protocol((1, 1))
